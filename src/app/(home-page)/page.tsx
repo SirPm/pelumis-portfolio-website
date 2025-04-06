3a4e742979b87e3e34f4dc85e0b38056961f634a
SirPm <odumosuoluwapm@gmail.com>
2025-04-06 14:50:47 +0100

    Initial setup

---
+++ src/app/(home-page)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+	return (
+		<div className="bg-amber-500">
+			<p className="text-white">welcome</p>
+		</div>
+	);
+}
