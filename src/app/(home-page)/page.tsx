1397850c1294c5f4cd723f27839cb9392c2d2eb7
SirPm <odumosuoluwapm@gmail.com>
2025-04-18 13:13:33 +0100

    - Move experience folder into homepage feature

--- src/app/(home-page)/page.tsx
+++ src/app/(home-page)/page.tsx
@@ -1,7 +1,7 @@
-import { Experience } from "@/features/experience/Experience";
-import { About } from "@/features/home-page/about/About";
 import { Header } from "@/features/home-page/header/Header";
 import { Projects } from "@/features/home-page/projects/Projects";
+import { About } from "@/features/home-page/about/About";
+import { Experience } from "@/features/home-page/experience/Experience";
 
 export default function Home() {
 	return (
