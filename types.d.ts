758be19252296bd5cf4bbb07c725a3149a52db2b
SirPm <odumosuoluwapm@gmail.com>
2026-04-11 10:21:58 +0100

    -fixed css import issue

--- types.d.ts
+++ types.d.ts
@@ -3,3 +3,8 @@ declare module "*.svg" {
 	const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
 	export default SVG;
 }
+
+declare module "*.css" {
+	const content: Record<string, string>;
+	export default content;
+}
